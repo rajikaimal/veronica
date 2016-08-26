126308246d0c3f7511ad9db3c610e0a9a3c3282b
rajikaimal <inbox.rajika@gmail.com>
2016-08-26 09:59:44 +0530

    tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,21 @@
 const expect = require('chai').expect;
+const veronica = require('../index');
 
 describe('test marvel API call', function() {
+	
 	it('should call marvel API', function(done) {
-		done();
+		veronica()
+			.then(res => {
+				expect(res).to.have.property('character');
+				done();
+			});
 	});
+
+	it('should give an error when calling to marvel API', function(done) {
+		veronica('iron man')
+			.then(res => {
+				expect(res).to.have.property('error');
+			});
+	});
+
 });
